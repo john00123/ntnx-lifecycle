b9b43f94289f45fe37531daa91ab5718323524dd
John Torres <john.torres.hurtado@gmail.com>
2017-12-10 22:15:58 -0800

    added tile logic

--- js/cards.js
+++ js/cards.js
@@ -1,29 +1,111 @@
 var cards = [
-{title:'Deck',
-category:'ahv',
-cluster: 'clustername',
-entities: 24,
-state:'In Progress'
+
+  {title:'Prism Central',
+  updated: '2 days ago',
+  category:'ahv',
+  cluster: 'all',
+  entities: 1,
+  state:'available'
+  },
+  {title:'Software component',
+  updated: '2 days ago',
+  category:'ahv',
+  cluster: 'clustername',
+  entities: 10,
+  state:'available'
+  },
+
+  {title:'Hypervisor',
+  updated: '2 days ago',
+  category:'ahv',
+  cluster: 'clustername',
+  entities: 30,
+  state:'available'
+  },
+
+  {title:'Acropolis OS',
+  updated: '2 days ago',
+  category:'ahv',
+  cluster: 'clustername',
+  entities: 10,
+  state:'In Progress'
+  },
+
+  {title:'Host Boot Devices',
+  updated: '2 days ago',
+  category:'ahv',
+  cluster: 'clustername',
+  entities: 30,
+  state:'In Progress'
+  },
+
+  {title:'NICs',
+  updated: '2 days ago',
+  category:'ahv',
+  cluster: 'clustername',
+  entities: 120,
+  state:'In Progress'
+  },
+
+  {title:'HBA',
+  updated: '2 days ago',
+  category:'ahv',
+  cluster: 'clustername',
+  entities: 30,
+  state:'In Progress'
+  },
+
+  {title:'BMC',
+  updated: '2 days ago',
+  category:'ahv',
+  cluster: 'clustername',
+  entities: 30,
+  state:'In Progress'
+  },
+
+  {title:'SATA Drives',
+  updated: '2 days ago',
+  category:'ahv',
+  cluster: 'clustername',
+  entities: 60,
+  state:'In Progress'
+  },
+
+  {title:'HDD Firmware',
+  updated: '2 days ago',
+  category:'ahv',
+  cluster: 'clustername',
+  entities: 60,
+  state:'In Progress'
+  },
+
+  {title:'File Server',
+  updated: '2 days ago',
+  category:'ahv',
+  cluster: 'clustername',
+  entities: 60,
+  state:'In Progress'
+  },
+];
+
+
+
+var buttons = [
+  {title: 'Update All',
+  class : 'primary-alt',
+  secondClass : 'multi-primary',
+  arrow: true
 },
 
-{title:'Deck',
-category:'ahv',
-cluster: 'clustername',
-entities: 24,
-state:'In Progress'
+  {title: 'Schedule',
+  class : 'secondary-alt',
+  secondClass : 'options-popup',
+  arrow: false
 },
 
-{title:'Deck',
-category:'ahv',
-cluster: 'clustername',
-entities: 24,
-state:'In Progress'
-},
-
-{title:'Deck',
-category:'ahv',
-cluster: 'clustername',
-entities: 24,
-state:'In Progress'
-}
+  {title: 'Options',
+  class : 'secondary-alt',
+  secondClass : 'options-popup',
+  arrow: false
+  }
 ];
--- js/script.js
+++ js/script.js
@@ -1,11 +1,94 @@
-
-let map1 = cards.map(card =>
+let deckAvailable = cards.filter(card => card.state === 'available')
+.map(card =>
   `<div class='card'>
-    <h5>${card.title}</h5>
-    <p>${card.state}</p>
+    <div class='card-header'>
+     <h5><input type='checkbox' class='select-tile'/> ${card.title} </h5>
+     <code> ${card.entities} updates </code>
+    </div>
+
+    <div class='card-body'>
+      <!-- <p>${card.state}</p> -->
+      <p>${ card.entities === 1 ?
+         card.entities + ` item` :
+         card.entities + ` items`}
+      </p>
+      <h4>Updated ${card.updated}</h4>
+      <a href='#'> details </a>
+    </div>
   </div>`
 );
 
+
+let deckUpdated = cards.filter(card => card.state !== 'available')
+.map(card =>
+  `<div class='card'>
+    <div class='card-header'>
+     <h5> ${card.title} </h5>
+    </div>
+
+    <div class='card-body'>
+      <!--<p>${card.state}</p>-->
+      <p>${card.entities} items</p>
+      <h4>Updated ${card.updated}</h4>
+      <a href='#'>details </a>
+    </div>
+  </div>`
+);
+
+
+//buttons
+
+let buttonGroup = buttons.map(button =>
+  button.arrow == false ?
+  `<button class= ${button.class} ${button.secondClass}>
+    ${button.title}
+  </button>`
+  :
+  `<div class =${button.secondClass}>
+    <button class= ${button.class}>${button.title}</button>
+    <button class='arrow'></button>
+  </div>`
+);
+
+
+//checkbox
+
+function checkCheck(){
+  let checkbox =$(this).find(".card-header h5 input");
+  let checked = checkbox.prop('checked') ? false : true;
+  checkbox.prop('checked', checked);
+  $('.selected-text').html($('input:checked').length !== 0 ? `/ ` +  $('input:checked').length + ` modules selected ${`<a class='clear'>clear</a>`}`: '');
+  $('.card-body a').click(event => event.stopPropagation());
+  $('.clear').click(clearCheck);
+ };
+
+function initialCheck(){
+  $('.card-body a').click();
+  $('.card:eq(0)').click();
+}
+
+function clearCheck(){
+  $('.card').find(".card-header h5 input").prop('checked', false);
+  $('.selected-text').html($('input:checked').length !== 0 ? `/ ` +  $('input:checked').length + ` modules selected ${`<a class='clear'>clear</a>`}`: '');
+}
+
+//filtering
+// function filtering(){
+//   let value = $('input').val();
+//   $('input').keydown(function(){
+//     $(`.section[data-type='available']`).html(deckUpdated);
+//   });
+// }
+
+
+//docready
+
 $(document).ready(function() {
-   $(`.section[data-type='available']`).append(map1);
+   $('.button-group').append(buttonGroup);
+   $(`.section[data-type='available']`).append(deckAvailable);
+   $(`.section[data-type='updated']`).append(deckUpdated);
+   $('.card').click(checkCheck);
+
+   // filtering();
+   initialCheck();
 });
